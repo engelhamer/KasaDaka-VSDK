dfe12e2074f27f9e7ef1765ed8cd3ec5d6f3db46
Engel Hamer <engelhamer@gmail.com>
2020-05-19 21:34:31 +0200

    Disable recording

--- vsdk/service_development/views/vse_retrieve_reports.py
+++ vsdk/service_development/views/vse_retrieve_reports.py
@@ -50,14 +50,14 @@ def get_reports(retrieve_element, session):
                 ).voice_label.get_voice_fragment_url(session.language),
                 'value': choice.choice_option_selected.voice_label.get_voice_fragment_url(session.language)
             })
-        for recording in user_report.recordings.all():
-            voice_report_content.append({
-                'voice_label': ReportContent.objects.get(
-                    parent=retrieve_element.report_element,
-                    content=recording.record_element
-                ).voice_label.get_voice_fragment_url(session.language),
-                'value': recording.get_voice_fragment_url(),
-            })
+        # for recording in user_report.recordings.all():
+        #     voice_report_content.append({
+        #         'voice_label': ReportContent.objects.get(
+        #             parent=retrieve_element.report_element,
+        #             content=recording.record_element
+        #         ).voice_label.get_voice_fragment_url(session.language),
+        #         'value': recording.get_voice_fragment_url(),
+        #     })
         voice_reports.append(voice_report_content)
 
     return filter_choices_selected, voice_reports
